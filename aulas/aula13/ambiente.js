3e06b92521fcbb5da5ccfa53fbb51a0366f362af
Vinícius da Silva <153554893+uvinicius@users.noreply.github.com>
2024-01-08 09:46:21 -0300

    aula 13

---
+++ aulas/aula13/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+while (c < 500){
+    console.log(`${c}`)
+    c++
+}
+do{
+    data = new Date()
+    agora = data.getMinutes()
+    console.log(`${agora}`)
+} while (agora <= 46)
+
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
